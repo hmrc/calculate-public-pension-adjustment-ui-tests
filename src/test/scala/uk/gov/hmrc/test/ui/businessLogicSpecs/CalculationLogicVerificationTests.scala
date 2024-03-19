975eb9544c4abafcef8b8b7e3cd3f1279fc25f5f
lahiru.wijesinghe@opencastsoftware.com <lahiru.wijesinghe@opencastsoftware.com>
2024-03-19 09:26:00 +0000

    add scenarios 2a

--- src/test/scala/uk/gov/hmrc/test/ui/businessLogicSpecs/CalculationLogicVerificationTests.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/businessLogicSpecs/CalculationLogicVerificationTests.scala
@@ -42,7 +42,8 @@ class CalculationLogicVerificationTests extends BaseSpec {
         "Scenario_26",
         "Scenario_25",
         "Scenario_1a",
-        "Scenario_1b"
+        "Scenario_1b",
+        "Scenario_2a"
       )
     requestArray.indices.foreach { index =>
       Scenario(s"Calculate Business Journey $index", ZapTests) {
