d23ea7c2e526becf0f2d03857f354486cf7be748
lahiru.wijesinghe@opencastsoftware.com <lahiru.wijesinghe@opencastsoftware.com>
2024-07-25 16:45:00 +0100

    add scalafm checks

--- run_tests_ISJ.sh
+++ run_tests_ISJ.sh
@@ -3,4 +3,5 @@
 BROWSER=$1
 ENVIRONMENT=$2
 
+sbt scalafmtCheckAll scalafmtSbtCheck
 sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.specs.* -- -n ISJJourney" testReport
