1a5a5cd8a961fcfb8f45a5b7385e55cf3cd676a5
swetaoza16 <sweta.oza@digital.hmrc.gov.uk>
2025-01-14 11:05:08 +0000

    updated dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8"   % Test,
     "org.scalatest"       %% "scalatest"       % "3.2.18"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"   % "3.2.17.0" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.42.0"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.43.0"   % Test,
     "com.typesafe.play"   %% "play-json"       % "2.10.0-RC8"
   )
 
