9aaa14a0046cb620cbb66a5c8647a85a00a6f224
swetaoza16 <sweta.oza@digital.hmrc.gov.uk>
2025-01-15 10:36:30 +0000

    selenium version and scala test version update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,4 +1,4 @@
-import sbt._
+import sbt.*
 
 object Dependencies {
 
@@ -7,7 +7,7 @@ object Dependencies {
     "com.typesafe"         % "config"          % "1.4.2"    % Test,
     "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8"   % Test,
     "org.scalatest"       %% "scalatest"       % "3.2.18"   % Test,
-    "org.scalatestplus"   %% "selenium-4-12"   % "3.2.17.0" % Test,
+    "org.scalatestplus"   %% "selenium-4-21"   % "3.2.19.0" % Test,
     "uk.gov.hmrc"         %% "ui-test-runner"  % "0.43.0"   % Test,
     "com.typesafe.play"   %% "play-json"       % "2.10.0-RC8"
   )
