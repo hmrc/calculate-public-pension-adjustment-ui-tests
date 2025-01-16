01fa0ae9bca1021f9ac634fc12bc85363c9b4655
neelu-dwivedi <141658439+neelu-dwivedi@users.noreply.github.com>
2025-01-16 13:47:22 +0000

    added timeout properties for jenkins fix

--- run_tests_local.sh
+++ run_tests_local.sh
@@ -4,4 +4,4 @@ BROWSER=$1
 ENVIRONMENT=$2
 
 sbt scalafmtCheckAll scalafmtSbtCheck
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Daccessibility.assessment=false -Dbrowser.logging=true -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.test.ui.specs.*" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" -Daccessibility.timeout="1000" -Dbrowser.logging=true -Dbrowser.option.headless=true "testOnly uk.gov.hmrc.test.ui.specs.*" testReport
